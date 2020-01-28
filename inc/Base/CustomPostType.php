2c543a71333eb527c458558d4c980159e256f60c
Arifin N <arifinofficial30@gmail.com>
2020-01-28 10:34:01 +0800

    add labels for custom post type

--- inc/Base/CustomPostType.php
+++ inc/Base/CustomPostType.php
@@ -18,13 +18,17 @@ class CustomPostType
         );
     
         $labels = array(
-            'name' => _x('Cota Rates', 'plural'),
-            'singular_name' => _x('Cota Rate', 'singular'),
+            'name' => __('Cota Rates', 'plural'),
+            'singular_name' => __('Cota Rate', 'singular'),
+            'add_new_item' => __('Add New Rate'),
+            'add_new' => __('Add New Rate'),
+            'edit_item' => __('Edit Rate'),
         );
     
         $args = array(
             'supports' => $supports,
             'labels' => $labels,
+            'public' => false,
             'query_var' => true,
             'show_in_menu' =>false,
             'show_ui' => true,
